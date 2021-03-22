2a11fc4bb27e9ee0a59f2c5db905110ead25b54c
glen15 <75651376+glen15@users.noreply.github.com>
2021-03-22 21:40:33 +0900

    6.2-Navigation

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About(){
+    return (
+        <div className="about__container">
+            <span>
+                About this page: I built it because I love movies.
+            </span>
+            <span> - tiltle of movie , years</span>
+        </div>
+    );
+}
+
+export default About;
